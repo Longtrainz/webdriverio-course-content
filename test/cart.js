56e3784ee5520ea92259a74e9e859f72f2d7d23f
vladyslav.bukach <vladyslav.bukach@globallogic.com>
2019-10-04 16:18:46 +0300

    Use $ command

--- test/cart.js
+++ test/cart.js
@@ -3,6 +3,12 @@ describe("Cart Functionality", function () {
     var btn = "#buyNowButton";
     var qty = "#qty";
 
+    var cart = {
+        get btn() { return $(btn); },
+        get qty() { return $(qty); },
+        get thankYou() { return $(".callout*=Thank you human") }
+    }
+
     // Go to the product page
     beforeEach(function() {
         browser.url('/product-page.html');
@@ -10,76 +16,73 @@ describe("Cart Functionality", function () {
 
     it ("should only let you buy after setting quantity", function () {
         // Verify 'Buy Now' button is initially disabled
-        var isBtnEnabled = browser.isEnabled(btn);
+        var isBtnEnabled = cart.btn.isEnabled();
         expect(isBtnEnabled, "'buy now' button should be disabled to begin").to.be.false;
 
         // Add qty
-        browser.setValue(qty, 10);
+        cart.qty.setValue(10);
 
         // Verify 'Buy Now' button is enabled after entering quantity
-        isBtnEnabled = browser.isEnabled(btn);
+        isBtnEnabled = cart.btn.isEnabled();
         expect(isBtnEnabled, "'buy now' button is now enabled").to.be.true;
     });
 
     describe("checkout process", function () {
         beforeEach(function() {
              // Add qty
-            browser.setValue(qty, 10);
+             cart.qty.setValue(10);
 
             // Click 'Buy Now' button
-            browser.click(btn);
+            cart.btn.click();
         });
 
         it("should disable 'Buy Now' button during processing", function () {
             // Verify 'Buy Now' button is disabled
-            var isBtnEnabled = browser.isEnabled(btn);
+            var isBtnEnabled = cart.btn.isEnabled();
             expect(isBtnEnabled, "'buy now' button should be disabled after clicking").to.be.false;
 
             // Verity button text is changed from 'Buy Now' to 'Purchasing'
-            var btnText = browser.getText(btn);
+            var btnText = cart.btn.getText();
             expect(btnText, "Verify 'buy now' test has changed").to.contain("Purchasing");
         });
 
         it("should show thank you message with qty and type", function () {
             // Verify thank you message is shown
-            var thankYou = ".callout*=Thank you human";
             // Alternative with XPath
             // var thankYou =  "//div[@class='callout' and contains(text(),'Thank you')]";
 
             // waitForExist "thank you message"
-            browser.waitForExist(thankYou, 3000);
+            cart.thankYou.waitForExist(3000);
 
             // verify it has properly qty and type
-            var thankText = browser.getText(thankYou);
+            var thankText =  cart.thankYou.getText();
             expect(thankText).to.contain("10 T-800 Model 101");
         });
 
         it("should clear input after completion", function () {
             // Verify 'Quantity' input is cleared
-            browser.waitForValue(qty, 3000, true);
+            cart.qty.waitForValue(3000, true);
         });
 
         it("should reset button text after purchase completes", function () {
             // Wait for button to return back to 'buy now'
             browser.waitUntil(function () {
-                return browser.getText(btn) !== 'Purchasing...';
+                return cart.btn.getText() !== 'Purchasing...';
             }, 3000);
 
-            var btnText = browser.getText(btn);
+            var btnText =  cart.btn.getText();
             expect(btnText).to.equal('Buy Now');
         });
 
         it("should hide thank you message after clicking close button", function () {
-            var thankYou = ".callout*=Thank you human";
-            
             // Verify thank you message is shown
-            browser.waitForExist(thankYou, 3000);
+            cart.thankYou.waitForExist();
 
             // Close thank you message
-            browser.click(".close-button");
+            $(".close-button").click();
 
             // Verify thank you message is not shown - use "reverse" flag to wait for it to disappear
-            browser.waitForVisible(thankYou, 3000, true);
+            cart.thankYou.waitForVisible(3000, true);
         });
     });
 });
